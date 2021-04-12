fbeffec629300f812f160993f10c54ec8bba4fde
PederFosse <peder.fosse@gmail.com>
2021-04-12 16:49:50 +0200

    Initial commit

---
+++ src/main/java/webprog/ukesoppgaver/UkesoppgaverApplication.java
@@ -0,0 +1,13 @@
+package webprog.ukesoppgaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkesoppgaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkesoppgaverApplication.class, args);
+    }
+
+}
